d5f96898a04ccb72bf5bfb4c95b70edb607dc457
Antoine D <antoine.desbordes@gmail.com>
2018-05-21 17:32:55 +0200

    call elasticsearch

--- api/pois.py
+++ api/pois.py
@@ -1,16 +1,26 @@
+from elasticsearch import Elasticsearch
+import os
 
 
-def fetch_es_poi(id, lang) -> dict:
+def fetch_es_poi(id) -> dict:
+    es_url = os.environ['ES_URL']  # TODO better configure this
+    es = Elasticsearch([es_url])
+    es_poi = es.get(id=id, index='munin_poi')
+
+    return es_poi['_source']
+
+
+def make_response(es_poi, lang) -> dict:
+    poi = {
+        'name': es_poi['name'],
+        'id': es_poi['id'],
+        'label': es_poi['label'],
+    }
     # TODO!
-    return {'name': 'toto', 'id': id, 'lang': lang}
-
-
-def make_response(es_poi):
-    # TODO!
-    return es_poi
+    return poi
 
 
 def get_poi(id, lang):
-    es_poi = fetch_es_poi(id, lang)
+    es_poi = fetch_es_poi(id)
 
-    return make_response(es_poi)
+    return make_response(es_poi, lang)
