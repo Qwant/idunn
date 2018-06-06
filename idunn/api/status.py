01fdf3fd05cc486a54862c3f1ebb126865d04ed2
Adrien Matissart <a.matissart@qwantresearch.com>
2018-06-06 18:20:20 +0200

    Handle ConnectionError when fetching cluster health

--- idunn/api/status.py
+++ idunn/api/status.py
@@ -1,10 +1,16 @@
-from elasticsearch import Elasticsearch
+import logging
+from elasticsearch import Elasticsearch, ConnectionError
 
 
 ES_RUNNING_STATUS = ('green', 'yellow')
 
 def get_status(es: Elasticsearch):
-    cluster_health = es.cluster.health()
+    try:
+        cluster_health = es.cluster.health()
+    except ConnectionError as err:
+        logging.error('Failed to connect to ES: %s', err)
+        cluster_health = {}
+
     return {
         'es': {
             'running': cluster_health.get('status') in ES_RUNNING_STATUS
