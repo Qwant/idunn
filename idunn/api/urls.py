d38246d91f2ea0dea5d7a22881d5402f7d5f0096
JBGriesner <jb.griesner@qwant.com>
2018-09-07 12:35:22 +0200

    add first prom endpoint

--- idunn/api/urls.py
+++ idunn/api/urls.py
@@ -3,8 +3,10 @@ from apistar import Route
 from .pois import get_poi
 from .status import get_status
 
+from apistar_prometheus import expose_metrics
 
 api_urls = [
+    Route('/metrics', 'GET', handler=expose_metrics),
     Route('/status', 'GET', handler=get_status),
     Route('/pois/{id}', 'GET', handler=get_poi),
 ]
