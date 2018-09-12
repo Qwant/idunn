fd35b625cac0c7ac99e0bbf750f08d4dd5c5e961
JBGriesner <jb.griesner@qwant.com>
2018-09-12 15:56:12 +0200

    simple test

---
+++ tests/test_metrics.py
@@ -0,0 +1,13 @@
+from unittest.mock import patch
+from apistar import TestClient
+
+from app import app
+
+
+def test_v1_metrics_ok():
+    client = TestClient(app)
+    response = client.get("http://localhost/v1/metrics")
+
+    assert response.status_code == 200
+
+    assert b'http_requests_inprogress{handler="apistar_prometheus.handlers.expose_metrics",method="GET"} 1.0' in response.content
