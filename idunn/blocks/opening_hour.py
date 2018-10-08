23d05756fe79cc7049785469b880838d4781c9b9
Adrien M <a@matissart.net>
2018-10-08 08:44:00 +0200

    WIP: Upgrade to hoh-1.0

--- idunn/blocks/opening_hour.py
+++ idunn/blocks/opening_hour.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta, date
 from pytz import timezone, UTC
 from apistar import validators, types
 from tzwhere import tzwhere
-from humanized_opening_hours.exceptions import ParseError, SpanOverMidnight
+from humanized_opening_hours.exceptions import HOHError
 
 from idunn.utils.opening_hours_lib import hoh
 from .base import BaseBlock
@@ -70,21 +70,21 @@ class OpeningHourBlock(BaseBlock):
 
         try:
             oh = hoh.OHParser(raw)
-            # Another hack in hoh: apply specific rules in priority
-            oh._tree.tree.children.sort(key=lambda x: len(x[0]))
-        except ParseError:
+            # # Another hack in hoh: apply specific rules in priority
+            # oh._tree.tree.children.sort(key=lambda x: len(x[0]))
+        except HOHError:
             logger.info("Failed to parse OSM opening_hour field", exc_info=True)
             return None
-        except SpanOverMidnight:
-            """
-            In the current hoh version opening hours are not allowed to span over midnight
-            However in the coming version (branch 'new-parsing' of the hoh repo:
-            https://github.com/rezemika/humanized_opening_hours/tree/new-parsing)
-            this feature will be supported
-            TODO: remove this catch when this will be released
-            """
-            logger.info("OSM opening_hour field cannot span over midnight", exc_info=True)
-            return None
+        # except SpanOverMidnight:
+        #     """
+        #     In the current hoh version opening hours are not allowed to span over midnight
+        #     However in the coming version (branch 'new-parsing' of the hoh repo:
+        #     https://github.com/rezemika/humanized_opening_hours/tree/new-parsing)
+        #     this feature will be supported
+        #     TODO: remove this catch when this will be released
+        #     """
+        #     logger.info("OSM opening_hour field cannot span over midnight", exc_info=True)
+        #     return None
 
         poi_tz = get_tz(es_poi)
         if poi_tz is None:
@@ -95,7 +95,12 @@ class OpeningHourBlock(BaseBlock):
         # The current version of the hoh lib doesn't allow to use the next_change() function
         # with an offset aware datetime.
         # This is why we replace the timezone info until this problem is fixed in the library.
-        nt = oh.next_change(allow_recursion=False, moment=poi_dt.replace(tzinfo=None))
+        try:
+            nt = oh.next_change(dt=poi_dt.replace(tzinfo=None))
+        except HOHError:
+            logger.info("Failed to compute next transition for poi %s",
+                es_poi.get('id'), exc_info=True)
+
 
         # Then we localize the next_change transition datetime in the local POI timezone.
         next_transition = poi_tz.localize(nt.replace(tzinfo=None))
@@ -105,7 +110,7 @@ class OpeningHourBlock(BaseBlock):
         delta = next_transition - poi_dt
         time_before_next = int(delta.total_seconds())
 
-        if oh.is_open(poi_dt):
+        if oh.is_open(poi_dt.replace(tzinfo=None)):
             status = 'open'
         else:
             status = 'closed'
@@ -115,7 +120,7 @@ class OpeningHourBlock(BaseBlock):
             next_transition_datetime=next_transition_datetime,
             seconds_before_next_transition=time_before_next,
             is_24_7=is247,
-            raw=oh.sanitized_field,
+            raw=oh.field,
             days=cls.get_days(oh, dt=poi_dt)
         )
 
@@ -138,16 +143,16 @@ class OpeningHourBlock(BaseBlock):
         for x in range(0,7):
             day = last_monday + timedelta(days=x)
             oh_day = oh_parser.get_day(day)
-            periods = oh_day.periods
+            periods = oh_day.opening_periods()
             day_value = {
                 'dayofweek': day.isoweekday(),
                 'local_date': day.isoformat(),
                 'status': 'open' if len(periods) > 0 else 'closed',
                 'opening_hours': []
             }
-            for p in periods:
-                beginning = cls.round_time_to_minute(p.beginning.time())
-                end = cls.round_time_to_minute(p.end.time())
+            for beginning_dt, end_dt in periods:
+                beginning = cls.round_time_to_minute(beginning_dt.time())
+                end = cls.round_time_to_minute(end_dt.time())
                 day_value['opening_hours'].append(
                     {
                         'beginning': beginning.strftime('%H:%M'),
--- idunn/utils/opening_hours_lib.py
+++ idunn/utils/opening_hours_lib.py
@@ -1,25 +1,25 @@
 import humanized_opening_hours as hoh
-from humanized_opening_hours.field_parser import WEEKDAYS, YearTransformer
+# from humanized_opening_hours.field_parser import WEEKDAYS, YearTransformer
 
-"""
-This is an ugly patch to work around a bug that caused
-an infinite loop when a day range overlaps the end of the week.
-eg: We-Mo
+# """
+# This is an ugly patch to work around a bug that caused
+# an infinite loop when a day range overlaps the end of the week.
+# eg: We-Mo
 
-This will be fixed in the next release of humanized_opening_hours,
-still in alpha to this day (2018-07-09)
-"""
-def patched_raw_consecutive_day_range(self, args):
-    """ Original implementation:
-    https://github.com/rezemika/humanized_opening_hours/blob/v0.6.2/humanized_opening_hours/field_parser.py#L70
-    """
-    first_day = WEEKDAYS.index(args[0])
-    last_day = WEEKDAYS.index(args[1])
-    if first_day <= last_day:
-        day_indexes = range(first_day, last_day+1)
-    else:
-        day_indexes = (i%7 for i in range(first_day, last_day+1+7))
-    return set(WEEKDAYS[i] for i in day_indexes)
+# This will be fixed in the next release of humanized_opening_hours,
+# still in alpha to this day (2018-07-09)
+# """
+# def patched_raw_consecutive_day_range(self, args):
+#     """ Original implementation:
+#     https://github.com/rezemika/humanized_opening_hours/blob/v0.6.2/humanized_opening_hours/field_parser.py#L70
+#     """
+#     first_day = WEEKDAYS.index(args[0])
+#     last_day = WEEKDAYS.index(args[1])
+#     if first_day <= last_day:
+#         day_indexes = range(first_day, last_day+1)
+#     else:
+#         day_indexes = (i%7 for i in range(first_day, last_day+1+7))
+#     return set(WEEKDAYS[i] for i in day_indexes)
 
-YearTransformer.raw_consecutive_day_range = patched_raw_consecutive_day_range
-hoh.field_parser.PARSER = hoh.field_parser.get_parser()
+# YearTransformer.raw_consecutive_day_range = patched_raw_consecutive_day_range
+# hoh.field_parser.PARSER = hoh.field_parser.get_parser()
