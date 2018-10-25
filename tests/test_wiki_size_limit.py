c3ba6c75c479dd6099c4df5076999285e5be42ab
JBGriesner <jb.griesner@qwant.com>
2018-10-25 14:53:22 +0200

    override settings max size

--- tests/test_wiki_size_limit.py
+++ tests/test_wiki_size_limit.py
@@ -4,6 +4,8 @@ import responses
 import pytest
 
 from .test_api import load_poi
+from .utils import override_settings
+from idunn.blocks.wikipedia import SizeLimiter
 
 @pytest.fixture(autouse=True)
 def louvre_museum(mimir_client):
@@ -12,6 +14,13 @@ def louvre_museum(mimir_client):
     in order that Idunn returns all possible blocks.
     """
     load_poi('louvre_museum.json', mimir_client)
+
+@pytest.fixture(scope="function")
+def wiki_max_size():
+    with override_settings({'WIKI_DESC_MAX_SIZE': 10}):
+        SizeLimiter._max_wiki_desc_size = None
+        yield
+    SizeLimiter._max_wiki_desc_size = None
 
 
 @pytest.fixture(scope='module', autouse=True)
@@ -35,21 +44,19 @@ def mock_long_wikipedia_response():
         rsps.add(
             responses.GET,
             'https://es.wikipedia.org/api/rest_v1/page/summary/Museo%20del%20Louvre',
-            json={  # This is a subset of the real response
+            json={
                 "extract": "El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia ..."
             }
         )
         yield
 
-
-def test_wiki_size_limit():
+def test_wiki_size_limit(wiki_max_size):
     client = TestClient(app)
     response = client.get(
         url=f'http://localhost/v1/pois/osm:relation:7515426?lang=es',
     )
-
     assert response.status_code == 200
-
     resp = response.json()
 
-    assert resp['blocks'][2].get('blocks')[0].get('description') == 'El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia El Museo del Louvre es el museo nacional de Francia El Museo del ...'
+    # Test that the wiki block description string is not longer than WIKI_DESC_MAX_SIZE (here only 10 chars)
+    assert resp['blocks'][2].get('blocks')[0].get('description') == 'El Museo d...'
