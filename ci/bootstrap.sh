94cc3e2b0e3b6eff117207e7b30b6f72a9865a04
Nicolas wallerand <n.wallerand@qwant.com>
2018-11-28 16:41:20 +0100

    CI-965 Deploy to the development environment

--- ci/bootstrap.sh
+++ ci/bootstrap.sh
@@ -10,6 +10,3 @@ alias make="make --warn-undefined-variables --no-keep-going"
 
 # source src/bootstrap.sh from GitLab
 source <(curl --silent --show-error "https://git.qwant.ninja/continuous-integration/gitlab-runner-bootstrap/raw/master/src/bootstrap.sh")
-
-export NO_PROXY="10.103.9.1,$NO_PROXY"
-export no_proxy="10.103.9.1,$no_proxy"
