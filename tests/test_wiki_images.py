ed3cd876c1762fd64b9bed9c3da3a425474d9df7
JBGriesner <jb.griesner@qwant.com>
2019-01-11 15:20:02 +0100

    url - 2 encode

--- tests/test_wiki_images.py
+++ tests/test_wiki_images.py
@@ -59,12 +59,12 @@ def test_orsay_images():
 
     assert resp["blocks"][4]["images"] == [
         {
-          "url": "https://qwant.com/thumbr/30x30/f/c/09ccd097b398526403a1bda0016d2e1385f0f9d7d2b069ee4b4221c9c5e3ed/1024px-Logo_musée_d'Orsay.png?u=https%253A%252F%252Fupload.wikimedia.org%252Fwikipedia%252Ffr%252Fthumb%252F7%252F73%252FLogo_mus%2525C3%2525A9e_d%252527Orsay.png%252F1024px-Logo_mus%2525C3%2525A9e_d%252527Orsay.png&q=0&b=1&p=0&a=0",
+          "url": "https://s2.qwant.com/thumbr/30x30/f/c/09ccd097b398526403a1bda0016d2e1385f0f9d7d2b069ee4b4221c9c5e3ed/1024px-Logo_musée_d'Orsay.png?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Ffr%2Fthumb%2F7%2F73%2FLogo_mus%25C3%25A9e_d%2527Orsay.png%2F1024px-Logo_mus%25C3%25A9e_d%2527Orsay.png&q=0&b=1&p=0&a=0",
           "alt": "Musée d'Orsay",
           "credits": "wikimedia"
         },
         {
-          "url": "https://qwant.com/thumbr/15x15/9/6/a33bf3566fe4bb2189c652aba5da1d3cac58b91eaa0ad0007acee4a33653bb/1024px-Logo_musée_d'Orsay.png?u=https%253A%252F%252Fupload.wikimedia.org%252Fwikipedia%252Ffr%252Fthumb%252F7%252F73%252FLogo_mus%2525C3%2525A9e_d%252527Orsay.png%252F1024px-Logo_mus%2525C3%2525A9e_d%252527Orsay.png&q=0&b=1&p=0&a=0",
+          "url": "https://s2.qwant.com/thumbr/15x15/9/6/a33bf3566fe4bb2189c652aba5da1d3cac58b91eaa0ad0007acee4a33653bb/1024px-Logo_musée_d'Orsay.png?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Ffr%2Fthumb%2F7%2F73%2FLogo_mus%25C3%25A9e_d%2527Orsay.png%2F1024px-Logo_mus%25C3%25A9e_d%2527Orsay.png&q=0&b=1&p=0&a=0",
           "alt": "Musée d'Orsay",
           "credits": "wikimedia"
         }
