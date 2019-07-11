bdac81db8ee18938ecdeb717375b6ac970ca5df1
Adrien Matissart <a.matissart@qwantresearch.com>
2019-07-11 10:47:05 +0200

    cleaner exception handling in IdunnApp

--- idunn/utils/app.py
+++ idunn/utils/app.py
@@ -41,19 +41,26 @@ class IdunnApp(App):
             # The `state` that caused the exception is not reused
             # to avoid polluting the injector cache
             # with partial dependency resolution.
-            state = get_default_state()
+            exc_state = get_default_state()
+            exc_state['exc'] = exc
+            if 'route' in state:
+                exc_state['route'] = state['route']
+            if 'path_params' in state:
+                exc_state['path_params'] = state['path_params']
+            return self.handle_request_exception(exc_state, on_response, on_error)
+
+    def handle_request_exception(self, state, on_response, on_error):
+        try:
+            funcs = (
+                [self.exception_handler] +
+                on_response +
+                [self.finalize_wsgi]
+            )
+            return self.injector.run(funcs, state)
+        except Exception as inner_exc:
             try:
-                state['exc'] = exc
-                funcs = (
-                    [self.exception_handler] +
-                    on_response +
-                    [self.finalize_wsgi]
-                )
+                state['exc'] = inner_exc
+                self.injector.run(on_error, state)
+            finally:
+                funcs = [self.error_handler, self.finalize_wsgi]
                 return self.injector.run(funcs, state)
-            except Exception as inner_exc:
-                try:
-                    state['exc'] = inner_exc
-                    self.injector.run(on_error, state)
-                finally:
-                    funcs = [self.error_handler, self.finalize_wsgi]
-                    return self.injector.run(funcs, state)
