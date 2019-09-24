485f769b281e86133ae88a916574d39c2eb57c14
Pierre Jackman <p.jackman@qwant.com>
2019-09-24 16:00:57 +0200

    QMAPS-1027 fix path to idunn-redis

--- ci/bootstrap.sh
+++ ci/bootstrap.sh
@@ -50,7 +50,7 @@ function deploy
 
     export DOCKER_IMAGE_DIGEST="$(cat idunn.digest)"
     export REDIS_IMAGE_DIGEST="$(docker-content-digest \
-        "$CI_PROJECT_PATH/postgis" "latest")"
+        "$CI_PROJECT_PATH/idunn-redis" "latest")"
 
     guess ENVIRONMENT from CI_JOB_NAME
     guess APP_HOST from CI_ENVIRONMENT_URL
