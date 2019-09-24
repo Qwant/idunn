957c31170250dc00c8197b41596afcd74e86243a
Pierre Jackman <p.jackman@qwant.com>
2019-09-24 15:28:54 +0200

    QMAPS-1027 clean up idunn deployment

--- ci/bootstrap.sh
+++ ci/bootstrap.sh
@@ -49,6 +49,8 @@ function deploy
     local STACK_FILE='docker-stack.yml'
 
     export DOCKER_IMAGE_DIGEST="$(cat idunn.digest)"
+    export REDIS_IMAGE_DIGEST="$(docker-content-digest \
+        "$CI_PROJECT_PATH/postgis" "latest")"
 
     guess ENVIRONMENT from CI_JOB_NAME
     guess APP_HOST from CI_ENVIRONMENT_URL
