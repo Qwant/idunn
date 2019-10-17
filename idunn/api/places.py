de6e051cf4db47b002c73156a591faca9a4605ee
Rémi Dupré <r.dupre@qwant.com>
2019-10-17 15:57:53 +0200

    api/places: enrich logs with human-readable POI informations

--- idunn/api/places.py
+++ idunn/api/places.py
@@ -9,6 +9,7 @@ from idunn import settings
 from idunn.utils import prometheus
 from idunn.utils.index_names import IndexNames
 from idunn.places import Place, Admin, Street, Address, POI, Latlon
+from idunn.places.base import BasePlace
 from idunn.api.utils import fetch_es_place, DEFAULT_VERBOSITY, ALL_VERBOSITY_LEVELS
 from idunn.api.pages_jaunes import pj_source
 from .closest import get_closest_place
@@ -31,18 +32,26 @@ def validate_lang(lang):
     return lang.lower()
 
 
-def log_custom_headers(id, headers: Headers):
-    custom_data = {}
+def log_place_request(place: BasePlace, headers: Headers):
+    custom_data = {
+        'id': place.get_id(),
+        'name': place.get_local_name(),
+        'class': place.get_class_name(),
+        'subclass': place.get_subclass_name(),
+    }
+
+    # Filter out irrelevant fields
+    for key in custom_data.keys():
+        if not custom_data[key]:
+            del custom_data[key]
+
     if 'X-QwantMaps-FocusPosition' in headers:
         pos = headers.get('X-QwantMaps-FocusPosition', '').split(';')
         if len(pos) == 3:
             try:
-                lon = float(pos[0])
-                lat = float(pos[1])
-                zoom = float(pos[2])
-                custom_data['lon'] = lon
-                custom_data['lat'] = lat
-                custom_data['zoom'] = zoom
+                custom_data['lon'] = float(pos[0])
+                custom_data['lat'] = float(pos[1])
+                custom_data['zoom'] = float(pos[2])
             except Exception:
                 logger.warning('Invalid data given through "X-QwantMaps-FocusPosition" header', exc_info=True)
     if 'X-QwantMaps-Query' in headers:
@@ -61,22 +70,25 @@ def log_custom_headers(id, headers: Headers):
         lang = headers.get('X-QwantMaps-QueryLang', '')
         if len(lang) > 0:
             custom_data['lang'] = lang
-    if len(custom_data) > 0:
-        custom_data['id'] = id
-        logger.info('Received details about user query', extra={'user_selection': custom_data})
+
+    logger.info(
+        'Received details about user query',
+        extra={'user_selection': custom_data}
+    )
 
 
 def get_place(id, es: Elasticsearch, indices: IndexNames, headers: Headers, lang=None, type=None, verbosity=DEFAULT_VERBOSITY) -> Place:
-    log_custom_headers(id, headers)
-
     """Main handler that returns the requested place"""
     verbosity = validate_verbosity(verbosity)
     lang = validate_lang(lang)
 
+    # Handle place from "pages jaunes"
     if id.startswith(pj_source.PLACE_ID_PREFIX):
         pj_place = pj_source.get_place(id)
+        log_place_request(pj_place, headers)
         return pj_place.load_place(lang, verbosity)
 
+    # Otherwise handle places from the ES db
     es_place = fetch_es_place(id, es, indices, type)
 
     places = {
@@ -91,7 +103,9 @@ def get_place(id, es: Elasticsearch, indices: IndexNames, headers: Headers, lang
         prometheus.exception("FoundPlaceWithWrongType")
         raise Exception("Place with id '{}' has a wrong type: '{}'".format(id, es_place[0].get('_type')))
 
-    return loader(es_place['_source']).load_place(lang, verbosity)
+    place = loader(es_place['_source'])
+    log_place_request(place, headers)
+    return place.load_place(lang, verbosity)
 
 
 def get_place_latlon(lat: float, lon: float, es: Elasticsearch, lang=None, verbosity=DEFAULT_VERBOSITY) -> Place:
