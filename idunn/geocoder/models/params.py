895db5538afebbcbaeca42816134b13ca2b8b090
Rémi Dupré <r.dupre@qwant.com>
2020-01-20 11:30:21 +0100

    autocomplete: use dataclass for params

--- idunn/geocoder/models/params.py
+++ idunn/geocoder/models/params.py
@@ -5,8 +5,10 @@ Bragi parameters as defined here:
 from enum import Enum
 from typing import List, Optional
 
+import dataclasses
 from fastapi import Query
 from pydantic import BaseModel, Field, PositiveInt, confloat, conint
+from pydantic.dataclasses import dataclass
 
 from idunn import settings
 from .cosmogony import ZoneType
@@ -21,55 +23,21 @@ class Type(str, Enum):
     Zone = "zone"
 
 
-class QueryParams(BaseModel):
-    q: str
-    lon: Optional[confloat(ge=-180, le=180)]
-    lat: Optional[confloat(ge=-90, le=90)]
-    lang: str
-    limit: PositiveInt
-    pt_dataset: List[str]
-    poi_dataset: List[str]
-    all_data: bool
-    offset: conint(gt=-1)
-    timeout: Optional[conint(gt=-1)]
-    type: List[Type]
-    zone_type: List[ZoneType]
-    poi_type: List[str]
-    _debug: bool
-
-    def __init__(
-        self,
-        q: str = Query(..., title="query string"),
-        lon: Optional[float] = Query(None, ge=-180, le=180, title="latitude for the focus"),
-        lat: Optional[float] = Query(None, ge=-90, le=90, title="longitude for the focus"),
-        lang: str = Query(settings["DEFAULT_LANGUAGE"], title="language"),
-        limit: PositiveInt = 10,
-        pt_dataset: List[str] = Query([]),
-        poi_dataset: List[str] = Query([]),
-        all_data: bool = False,
-        offset: conint(gt=-1) = 0,
-        timeout: Optional[conint(gt=-1)] = None,
-        type: List[Type] = Query([]),
-        zone_type: List[ZoneType] = Query([]),
-        poi_type: List[str] = Query([]),
-        _debug: bool = False,
-    ):
-        super().__init__(
-            q=q,
-            lon=lon,
-            lat=lat,
-            lang=lang,
-            limit=limit,
-            pt_dataset=pt_dataset,
-            poi_dataset=poi_dataset,
-            all_data=all_data,
-            offset=offset,
-            timeout=timeout,
-            type=type,
-            zone_type=zone_type,
-            poi_type=poi_type,
-            _debug=_debug,
-        )
+@dataclass
+class QueryParams:
+    q: str = Query(..., title="query string")
+    lon: Optional[confloat(ge=-180, le=180)] = Query(None, title="latitude for the focus")
+    lat: Optional[confloat(ge=-90, le=90)] = Query(None, title="longitude for the focus")
+    lang: str = Query(settings["DEFAULT_LANGUAGE"], title="language")
+    limit: PositiveInt = 10
+    pt_dataset: List[str] = Query([])
+    poi_dataset: List[str] = Query([])
+    all_data: bool = False
+    offset: conint(gt=-1) = 0
+    timeout: Optional[conint(gt=-1)] = None
+    type: List[Type] = Query([])
+    zone_type: List[ZoneType] = Query([])
+    poi_type: List[str] = Query([])
 
     def bragi_query_dict(self):
         """
@@ -79,7 +47,7 @@ class QueryParams(BaseModel):
         """
         return {
             (key if not isinstance(value, list) else key + "[]"): value
-            for (key, value) in self.dict().items()
+            for (key, value) in dataclasses.asdict(self).items()
         }
 
 
