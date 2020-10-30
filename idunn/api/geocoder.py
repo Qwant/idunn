7f37450c865e0fbd55640710628fe0573435bb36
Adrien Matissart <a.matissart@qwantresearch.com>
2020-10-30 16:48:52 +0100

    autocomplete: Exclude implausible intentions with full-text query only and few matching features

--- idunn/api/geocoder.py
+++ idunn/api/geocoder.py
@@ -12,6 +12,25 @@ nlu_allowed_languages = settings["NLU_ALLOWED_LANGUAGES"].split(",")
 autocomplete_nlu_shadow_enabled = settings["AUTOCOMPLETE_NLU_SHADOW_ENABLED"]
 
 
+def post_filter_intention(intention, bragi_response, limit):
+    if not intention.description.query or intention.description.place:
+        return True
+
+    # Detected intention is a full-text query only (brand).
+    # A generic brand query will typically match dozens of POIs.
+    # To avoid false positives, let's ignore the intention if matching
+    # results among geocoded features seem to be too rare.
+    expected_matches = limit / 2
+    matches = 0
+    for f in bragi_response["features"]:
+        name = f.get("properties", {}).get("geocoding", {}).get("name", "")
+        if nlu_client.fuzzy_match(intention.description.query, name):
+            matches += 1
+            if matches > expected_matches:
+                return True
+    return False
+
+
 async def get_autocomplete(
     query: QueryParams = Depends(QueryParams), extra: ExtraParams = Body(ExtraParams())
 ):
@@ -32,5 +51,10 @@ async def get_autocomplete(
         bragi_client.autocomplete(query, extra), get_intentions()
     )
     if intentions is not None and query.nlu:
+        intentions = list(
+            filter(
+                lambda i: post_filter_intention(i, autocomplete_response, query.limit), intentions
+            )
+        )
         autocomplete_response["intentions"] = intentions
     return autocomplete_response
