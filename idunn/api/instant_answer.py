4d79c10f9dae6f60a51cd2af9d2b91367e60ef36
Rémi Dupré <r.dupre@qwant.com>
2021-02-08 10:43:50 +0100

    instant answer: more permissive filter for addresses

--- idunn/api/instant_answer.py
+++ idunn/api/instant_answer.py
@@ -1,14 +1,18 @@
 import logging
+import re
 from fastapi import HTTPException, Query
 from fastapi.responses import ORJSONResponse
 from fastapi.concurrency import run_in_threadpool
 from typing import Optional, List, Tuple
 from pydantic import BaseModel, Field, validator, HttpUrl
+from jellyfish import damerau_levenshtein_distance
+from unidecode import unidecode
 
 from idunn import settings
 from idunn.geocoder.nlu_client import nlu_client, NluClientException
 from idunn.geocoder.bragi_client import bragi_client
 from idunn.places import place_from_id, Place
+from idunn.api.places import PlaceType
 from idunn.api.places_list import get_places_bbox
 from idunn.utils import maps_urls
 from idunn.instant_answer import normalize
@@ -67,6 +71,152 @@ class InstantAnswerResponse(BaseModel):
 
 
 NoInstantAnswerToDisplay = HTTPException(status_code=404, detail="No instant answer to display")
+NUM_SUFFIXES = ["bis", "ter", "quad", "e", "eme", "ème", "er", "st", "nd", "rd", "th"]
+
+
+class PlaceFilter(BaseModel):
+    type: PlaceType
+    name: str
+    postcodes: List[str] = []
+    admins: List[str] = []
+
+    @staticmethod
+    def from_bragi_type(bragi_type):
+        if bragi_type == "house":
+            return PlaceType.ADDRESS
+        if bragi_type == "poi":
+            return PlaceType.POI
+        if bragi_type == "street":
+            return PlaceType.STREET
+        if bragi_type in ("city", "zone"):
+            return PlaceType.ADMIN
+        raise Exception(f"unsupported type `{bragi_type}`")
+
+    @classmethod
+    def from_bragi_response(cls, bragi_response):
+        if bragi_response["postcode"]:
+            postcodes = bragi_response["postcode"].split(";")
+        else:
+            postcodes = list(
+                {
+                    z
+                    for admin in bragi_response["administrative_regions"]
+                    for z in admin["zip_codes"]
+                }
+            )
+
+        return PlaceFilter(
+            type=cls.from_bragi_type(bragi_response["type"]),
+            name=bragi_response["name"],
+            postcodes=postcodes,
+            admins=[admin["name"] for admin in bragi_response["administrative_regions"]],
+        )
+
+    @staticmethod
+    def words(text):
+        """
+        Split words into a list.
+
+        >>> PlaceFilter.words("17, rue jaune ; Levallois-Peret")
+        ['17', 'rue', 'jaune', 'Levallois', 'Peret']
+        """
+        separators = [" ", "-", ",", ";"]
+        return [word for word in re.split("|".join(separators), text) if word]
+
+    @staticmethod
+    def word_as_number(word):
+        """
+        Attempt to intepret the word as a number, potentialy triming a suffix.
+
+        >>> PlaceFilter.word_as_number("17")
+        '17'
+        >>> PlaceFilter.word_as_number("17bis")
+        '17'
+        >>> PlaceFilter.word_as_number("17rue") is None
+        True
+        """
+        if word.isnumeric():
+            return word
+
+        for suffix in NUM_SUFFIXES:
+            if word.lower().endswith(suffix):
+                prefix = word[: -len(suffix)]
+
+                if prefix.isnumeric():
+                    return prefix
+
+        return None
+
+    @classmethod
+    def word_matches(cls, query_word, label_word):
+        """
+        Check if two words match with an high degree of confidence.
+
+        A single spelling mistake is allowed, defined as either a wrong letter
+        (insertion, deletion or replaced) or the inversion of two consecutive
+        letters. If an accent is defined in the query, then it must appear in
+        the result (or it will count as a spelling mistake).
+
+        Numbers have a special treatment: if word from the query is a number
+        (without potential suffix), then it must exactly match the word from
+        the result (a number, potentialy with suffix removed).
+
+        >>> PlaceFilter.word_matches("42ter", "42")
+        True
+        >>> PlaceFilter.word_matches("42ter", "42bis")
+        True
+        >>> PlaceFilter.word_matches("321", "322")
+        False
+
+        >>> PlaceFilter.word_matches("eiffel", "ieffel")
+        True
+        >>> PlaceFilter.word_matches("eiffel", "ifefel")
+        False
+        >>> PlaceFilter.word_matches("eveque", "évêque")
+        True
+        >>> PlaceFilter.word_matches("évêque", "eveque")
+        False
+        """
+        if (query_word_as_num := cls.word_as_number(query_word)) is not None:
+            return cls.word_as_number(label_word) == query_word_as_num
+
+        return any(
+            damerau_levenshtein_distance(query_word.lower(), s) <= 1
+            for s in [label_word.lower(), unidecode(label_word.lower())]
+        )
+
+    def filter(self, query):
+        if self.type == PlaceType.ADDRESS:
+            return self.filter_address(query)
+
+        return self.name.lower() == query.lower()
+
+    def filter_address(self, query):
+        query = [
+            word
+            for word in self.words(query)
+            if (len(word) > 2 or word.isnumeric()) and word.lower() not in NUM_SUFFIXES
+        ]
+
+        if len(query) <= 2:
+            return False
+
+        full_label = [
+            *self.words(self.name),
+            *self.postcodes,
+            *(w for admin in self.admins for w in self.words(admin)),
+        ]
+
+        for q_word in query:
+            if not any(self.word_matches(q_word, l_word) for l_word in full_label):
+                logger.warning(
+                    "Removed `%s` from results because queried `%s` does not match the result",
+                    self.name,
+                    q_word,
+                )
+                return False
+
+        return True
 
 
 def build_response(result: InstantAnswerResult, query: str, lang: str):
@@ -130,9 +280,8 @@ async def get_instant_answer(
         if len(bragi_response["features"]) > 0:
             place_geocoding = bragi_response["features"][0]["properties"]["geocoding"]
             place_id = place_geocoding["id"]
-            place_name = place_geocoding["name"]
-            # TODO: use smarter condition to allow inexact matches
-            if place_name.lower() == normalized_query:
+            place_filter = PlaceFilter.from_bragi_response(place_geocoding)
+            if place_filter.filter(normalized_query):
                 result = await run_in_threadpool(
                     get_instant_answer_single_place, place_id=place_id, lang=lang
                 )
