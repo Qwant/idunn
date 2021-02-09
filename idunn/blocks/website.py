dae64d1cdabd43bd7022d6cb39302ff6ad0dbc8e
Adrien Matissart <a.matissart@qwantresearch.com>
2021-02-09 17:25:39 +0100

    Handle more contact tags: 'contact:mobile', 'contact:facebook'

--- idunn/blocks/website.py
+++ idunn/blocks/website.py
@@ -1,12 +1,16 @@
+import logging
 from urllib.parse import urlparse
 from typing import Literal
+from pydantic import ValidationError, AnyHttpUrl
 
 from .base import BaseBlock
+
+logger = logging.getLogger(__name__)
 
 
 class WebSiteBlock(BaseBlock):
     type: Literal["website"] = "website"
-    url: str
+    url: AnyHttpUrl
     label: str
 
     @classmethod
@@ -21,4 +25,13 @@ class WebSiteBlock(BaseBlock):
             # Extract domain name or hostname from website
             label = urlparse(website).netloc
 
-        return cls(url=website, label=label)
+        try:
+            return cls(url=website, label=label)
+        except ValidationError:
+            logger.debug(
+                "Website block is invalid for %s. Website: %s",
+                place.get_id(),
+                website,
+                exc_info=True,
+            )
+            return None
