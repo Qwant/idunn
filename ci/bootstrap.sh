cdb715754d7717602f34acf0643a618e9499d2dc
Pierre Jackman <p.jackman@qwant.com>
2021-04-02 15:43:55 +0200

    QMAPS-2021 deploy classifier to kube maps namespace

--- ci/bootstrap.sh
+++ ci/bootstrap.sh
@@ -1,48 +1,10 @@
 #! /bin/bash
 set -euo pipefail
 shopt -s expand_aliases
-export PATH="$PWD/ci/bin:$PWD/bin:$PATH"
 
-# avoid undefined variables in Makefiles
-alias make="make --warn-undefined-variables --no-keep-going"
+source ci/bootstrap/src/bootstrap.sh
 
-# source src/bootstrap.sh from GitLab
-source <(curl --silent --show-error "https://git.qwant.ninja/continuous-integration/gitlab-runner-bootstrap/raw/87c2437224964ce7f40680dff37471a329d46d76/src/bootstrap.sh")
-
-# Guess some variables
-function guess # <variable name> from <variable name>
-{
-    function process # <value>
-    {
-        if [[ $? == 0 ]]
-        then
-            echo "Guessing ${FUNCNAME[1]}: $1" >&2
-            export ${FUNCNAME[1]}="$1"
-        fi
-    }
-
-    function APP_HOST # from CI_ENVIRONMENT_URL
-    {
-        process $(sed -E 's|^(http[s]?://)?([^/]+).*$|\2|g' <<< "${!2}")
-    }
-
-    function ENVIRONMENT # from CI_JOB_NAME
-    {
-        process $(grep -Po '(review|development|prelive|production)' <<< "${!2}")
-    }
-
-    "$@"
-}
-
-function url
-{
-    function host
-    {
-        sed -E 's|^(tcp://)?([^/:]+).*(:\d+)?$|\2|g' <<< "$1"
-    }
-
-    "$@"
-}
+export PATH="$PWD/ci/bootstrap/bin:$PWD/bin:$PATH"
 
 function deploy
 {
@@ -61,5 +23,5 @@ function deploy
     docker service update --network-add "${STACK_NAME}_default" "$ROUTER_SERVICE_NAME" || true
     docker service update --network-add "${STACK_NAME}_default" "$PROMETHEUS_SERVICE_NAME" || true
     docker service update --network-add "${STACK_NAME}_default" "$NLU_SERVICE_NAME" || true
-    docker-stack-wait -t 600 "$STACK_NAME"
+    docker-stack-deploy-wait -t 600 "$STACK_NAME"
 }
