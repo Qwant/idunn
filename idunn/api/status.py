df94c11bf6135c76458b59864d4c0ca6a9bb53d3
fatal69100 <sebastiendrll@protonmail.com>
2022-02-01 09:43:16 +0100

    Catch exception on healthcheck timeout (#304)
    * catch exception on healthcheck
    
    * fix format
    
    * log error
    
    Co-authored-by: sdirollo <sebastien.dirollo@gmail.com>

--- idunn/api/status.py
+++ idunn/api/status.py
@@ -17,14 +17,21 @@ class RequestsHTTPError:
     pass
 
 
+logger = logging.getLogger(__name__)
+
+
 def get_status():
     """Returns the status of the elastic cluster"""
     es_mimir_status = get_es_status(get_mimir_elasticsearch())
 
-    wiki_es_response = requests.get(settings["WIKI_ES"], timeout=2)
-    if wiki_es_response.status_code == 200:
-        es_wiki_status = "up"
-    else:
+    try:
+        wiki_es_response = requests.get(settings["WIKI_ES"], timeout=2)
+        if wiki_es_response.status_code == 200:
+            es_wiki_status = "up"
+        else:
+            es_wiki_status = "down"
+    except Exception as e:
+        logger.error("Wiki es connection error: %s", e)
         es_wiki_status = "down"
 
     info = WikiEs().get_info("Q7652", "fr")
@@ -44,16 +51,28 @@ def get_status():
         "lowercase": True,
     }
 
-    tagger_response = requests.post(settings["NLU_TAGGER_URL"], json=tagger_body_dict, timeout=2)
-    if tagger_response.status_code == 200:
-        tagger_status = "up"
-    else:
+    try:
+        tagger_response = requests.post(
+            settings["NLU_TAGGER_URL"], json=tagger_body_dict, timeout=2
+        )
+        if tagger_response.status_code == 200:
+            tagger_status = "up"
+        else:
+            tagger_status = "down"
+    except Exception as e:
+        logger.error("Tagger connection error: %s", e)
         tagger_status = "down"
 
-    classifier_response = requests.post(settings["NLU_CLASSIFIER_URL"], json=classifier_body_dict, timeout=2)
-    if classifier_response.status_code == 200:
-        classifier_status = "up"
-    else:
+    try:
+        classifier_response = requests.post(
+            settings["NLU_CLASSIFIER_URL"], json=classifier_body_dict, timeout=2
+        )
+        if classifier_response.status_code == 200:
+            classifier_status = "up"
+        else:
+            classifier_status = "down"
+    except Exception as e:
+        logger.error("Classifier connection error: %s", e)
         classifier_status = "down"
 
     try:
@@ -62,10 +81,16 @@ def get_status():
     except PlaceNotFound:
         pj_status = "down"
 
-    response = requests.get(settings["BRAGI_BASE_URL"] + "/status", verify=settings["VERIFY_HTTPS"], timeout=2)
-    if "version" in response.json()["bragi"]:
-        bragi_status = "up"
-    else:
+    try:
+        response = requests.get(
+            settings["BRAGI_BASE_URL"] + "/status", verify=settings["VERIFY_HTTPS"], timeout=2
+        )
+        if "version" in response.json()["bragi"]:
+            bragi_status = "up"
+        else:
+            bragi_status = "down"
+    except Exception as e:
+        logger.error("Bragi connection error: %s", e)
         bragi_status = "down"
 
     return {
