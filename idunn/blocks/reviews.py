b8dd13c50a3821e95c0942521aa2963ecd78053c
sdirollo <sebastien.dirollo@gmail.com>
2022-04-15 15:35:16 +0200

    remove review block pydantic validator

--- idunn/blocks/reviews.py
+++ idunn/blocks/reviews.py
@@ -21,28 +21,28 @@ class Review(BaseModel):
     trip_type: Optional[str]
     author_name: str
 
-    @validator("rating_bubble_star_url", pre=True, always=True)
-    def build_rating_bubble_star_url(cls, rating):
-        # Tripadvisor bubble star url need a rating with exactly one decimal point
-        # (e.g 4.0 or 4.5)
-        rating = f"{float(rating):.1f}"
 
-        base_url = (
-            r"https://www.tripadvisor.com/img/cdsi/img2/ratings/traveler/"
-            f"s{rating}-MCID-66562.svg"
-        )
+def build_rating_bubble_star_url(rating):
+    # Tripadvisor bubble star url need a rating with exactly one decimal point
+    # (e.g 4.0 or 4.5)
+    rating = f"{float(rating):.1f}"
 
-        if thumbr.is_enabled():
-            return thumbr.get_url_remote_thumbnail(base_url)
+    base_url = (
+        r"https://www.tripadvisor.com/img/cdsi/img2/ratings/traveler/"
+        f"s{rating}-MCID-66562.svg"
+    )
 
-        return base_url
+    if thumbr.is_enabled():
+        return thumbr.get_url_remote_thumbnail(base_url)
+
+    return base_url
 
 
 def build_review(review: dict, source_url: str) -> Review:
     return Review(
         date=review["DatePublished"],
         rating=review["Rating"],
-        rating_bubble_star_url=review["Rating"],
+        rating_bubble_star_url=build_rating_bubble_star_url(review["Rating"]),
         url="".join([source_url, review["ReviewURL"]]),
         more_reviews_url="".join([source_url, review["MoreReviewsURL"]]),
         lang=review["Language"],
@@ -77,6 +77,5 @@ class ReviewsBlock(BaseBlock):
     def from_es(cls, place, lang: str):
         if place.get_reviews() is None:
             return None
-
         reviews = cls.build_reviews(place.get_reviews(), place.get_source_url(), lang)
         return cls(reviews=reviews)
