b7a27e574518cf4cc4edfeae106802d5bd2d1b3d
sdirollo <sebastien.dirollo@gmail.com>
2022-04-21 11:18:26 +0200

    group end-to-end tests

--- end_to_end/instant_answer.py
+++ end_to_end/instant_answer.py
@@ -1,5 +1,6 @@
 import os
 
+import pytest
 from fastapi.testclient import TestClient
 
 os.environ['IS_TEST'] = 'true'
@@ -16,54 +17,50 @@ def assert_autocomplete(query: str, doc: str):
     assert response.json()['data']['result']['places'][0]['id'] == doc
 
 
-def test_admin_paris():
-    assert_autocomplete("http://localhost:5000/v1/instant_answer?q=paris", 'admin:osm:relation:71525')
+class TestAdmin:
+    @pytest.mark.parametrize("query, expected_id", [
+        ("paris", 'admin:osm:relation:71525'),
+        ("Bruges", 'admin:osm:relation:562654'),
+        ("La Balme-de-Sillingy", 'admin:osm:relation:103823'),
+        ("Megève", 'admin:osm:relation:75312'),
+        ("Saffré", 'admin:osm:relation:173715'),
+    ])
+    def test_admin(self, query, expected_id):
+        assert_autocomplete(f"http://localhost:5000/v1/instant_answer?q={query}", expected_id)
 
 
-def test_admin_bruges():
-    assert_autocomplete("http://localhost:5000/v1/instant_answer?q=Bruges", 'admin:osm:relation:562654')
+class TestShouldCallTripadvisor:
+    def test_when_hotel_category_is_detected(self):
+        assert_autocomplete("http://localhost:5000/v1/instant_answer?q=hotel gabriel paris", 'ta:poi:529918')
 
 
-def test_admin_la_balme():
-    assert_autocomplete("http://localhost:5000/v1/instant_answer?q=La Balme-de-Sillingy", 'admin:osm:relation:103823')
+    def test_when_poi_without_place_intention(self):
+        assert_autocomplete("http://localhost:5000/v1/instant_answer?q=garden bistro", 'ta:poi:1509459')
 
 
-def test_admin_megeve():
-    assert_autocomplete("http://localhost:5000/v1/instant_answer?q=Megève", 'admin:osm:relation:75312')
+    def test_when_restaurant_without_place_intention(self):
+        assert_autocomplete("http://localhost:5000/v1/instant_answer?q=café rive droite", 'ta:poi:1540773')
 
 
-def test_admin_saffre():
-    assert_autocomplete("http://localhost:5000/v1/instant_answer?q=Saffré", 'admin:osm:relation:173715')
+class TestShouldCallPagesJaunes:
+    def test_when_restaurant_with_france_place_intention(self):
+        assert_autocomplete("http://localhost:5000/v1/instant_answer?q=garden bistro lyon", 'pj:58140933')
+
+    def test_when_poi_with_place_in_france(self):
+        assert_autocomplete("http://localhost:5000/v1/instant_answer?q=ecole maternelle jacques prevert orléans", 'pj:12359795')
 
 
-def test_mdph():
-    assert_autocomplete("http://localhost:5000/v1/instant_answer?q=Direction de la Solidarité Départementale - MDPH",
-                        'osm:relation:1672143')
+class TestShouldCallOSM:
+    @pytest.mark.parametrize("query, expected_id", [
+        ("tour eiffel", 'osm:way:5013364'),
+        ("groupama stadium", 'osm:way:848361602'),
+    ])
+    def test_when_poi_link_with_wikidata_id(self, query, expected_id):
+        assert_autocomplete(f"http://localhost:5000/v1/instant_answer?q={query}", expected_id)
 
+    @pytest.mark.parametrize("query, expected_id", [
+        ("Direction de la Solidarité Départementale - MDPH", 'osm:relation:1672143')
+    ])
+    def test_fallback_osm(self, query, expected_id):
+        assert_autocomplete(f"http://localhost:5000/v1/instant_answer?q={query}", expected_id)
 
-def test_hotel_should_call_tripadvisor_when_hotel_category_is_detected():
-    assert_autocomplete("http://localhost:5000/v1/instant_answer?q=hotel gabriel paris", 'ta:poi:529918')
-
-
-def test_restaurant_with_france_place_intention_should_call_pagesjaunes_for_poi():
-    assert_autocomplete("http://localhost:5000/v1/instant_answer?q=garden bistro lyon", 'pj:58140933')
-
-
-def test_poi_without_place_intention_should_call_tripadvisor_when_found():
-    assert_autocomplete("http://localhost:5000/v1/instant_answer?q=garden bistro", 'ta:poi:1509459')
-
-
-def test_restaurant_without_place_intention_should_call_tripadvisor_when_found():
-    assert_autocomplete("http://localhost:5000/v1/instant_answer?q=café rive droite", 'ta:poi:1540773')
-
-#TODO to fix
-def test_osm_for_famous_poi():
-    assert_autocomplete("http://localhost:5000/v1/instant_answer?q=tour eiffel", 'osm:way:5013364')
-
-
-def test_attraction_should_call_tripadvisor():
-    assert_autocomplete("http://localhost:5000/v1/instant_answer?q=groupama stadium", 'osm:way:848361602')
-
-
-def test_fallback_osm():
-    assert_autocomplete("http://localhost:5000/v1/instant_answer?q=ecole maternelle jacques prevert orléans", 'osm:way:810143256')
