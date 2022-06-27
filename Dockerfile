d92519564fc3cf1b9a838c3701dbb1de469d8b64
Aureliano Sinatra <a.sinatra@qwant.com>
2022-06-27 10:58:03 +0200

    use alpine instead of debian

--- Dockerfile
+++ Dockerfile
@@ -1,31 +1,59 @@
-FROM python:3.10-slim
+# ---
+# --- Builder image
+# ---
 
-RUN apt-get update && apt-get -y install git gcc
+FROM python:3.10-alpine as builder
 
-RUN useradd -r app_user
-RUN mkdir /app
-RUN chown app_user /app/
-WORKDIR /app
+WORKDIR /home/idunn
 
-# Installing packages
+# Install build dependancies
+RUN apk update && apk add --upgrade --no-cache g++ make
+
+# Install pipenv
+RUN pip install --no-cache-dir --upgrade pip
 RUN pip install pipenv
 
-ADD --chown=app_user app.py Pipfile* /app/
-RUN pipenv install --system --deploy
+# Build a venv with dependancies in current directory
+ADD Pipfile.lock Pipfile* /home/idunn/
+RUN PIPENV_VENV_IN_PROJECT=1 pipenv sync
 
-USER app_user
+# ---
+# --- Application image
+# ---
 
-# the sources are copied as late as possible since they are likely to change often
-ADD --chown=app_user idunn /app/idunn
+FROM python:3.10-alpine
 
-# set the multiprocess mode for gunicorn
+ENV PYTHONUNBUFFERED=1
+
+# Set the multiprocess mode for gunicorn
 ENV IDUNN_PROMETHEUS_MULTIPROC=1
-ENV PROMETHEUS_MULTIPROC_DIR=/app/idunn/prometheus_multiproc
-RUN mkdir /app/idunn/prometheus_multiproc
+ENV PROMETHEUS_MULTIPROC_DIR=/home/idunn/prometheus_multiproc
+RUN mkdir -p /home/idunn/prometheus_multiproc
+
+# Install lib dependancies
+RUN apk update && apk add --upgrade --no-cache geos
+
+# Create the user idunn
+RUN addgroup --gid 1000 idunn
+RUN adduser --disabled-password --home /home/idunn --ingroup idunn \
+            --uid 1000 idunn
+
+USER idunn
+WORKDIR /home/idunn
+
+# Add files into images
+ADD --chown=idunn app.py /home/idunn
+ADD --chown=idunn idunn /home/idunn/idunn
+COPY --chown=idunn --from=builder /home/idunn/.venv /home/idunn/.venv
 
 EXPOSE 5000
 
 # You can set the number of workers by passing --workers=${NB_WORKER} to the docker run command.
 # For some reason, an array is required here to accept other params on run.
-ENTRYPOINT ["gunicorn", "app:app", "--bind=0.0.0.0:5000", "--pid=/tmp/gunicorn.pid", \
-    "-k", "uvicorn.workers.UvicornWorker", "--preload"]
+ENTRYPOINT [                                           \
+    "./.venv/bin/python", "-m", "gunicorn", "app:app", \
+    "--bind=0.0.0.0:5000",                             \
+    "--pid=/tmp/gunicorn.pid",                         \
+    "-k", "uvicorn.workers.UvicornWorker",             \
+    "--preload"                                        \
+]
--- tests/test_directions.py
+++ tests/test_directions.py
@@ -18,8 +18,7 @@ from .utils import override_settings
 def mock_directions_car():
     with override_settings(
         {
-            "QWANT_DIRECTIONS_API_BASE_URL": "http://api.qwant/directions",
-            "MAPBOX_DIRECTIONS_ACCESS_TOKEN": None,
+            "MAPBOX_DIRECTIONS_ACCESS_TOKEN": "test",
         }
     ):
         fixture_path = os.path.join(
@@ -30,7 +29,7 @@ def mock_directions_car():
         with responses.RequestsMock() as rsps:
             rsps.add(
                 "GET",
-                re.compile(r"^http://api.qwant/directions/"),
+                re.compile(r"^https://api.mapbox.com/"),
                 status=200,
                 json=json.load(open(fixture_path)),
             )
@@ -45,24 +44,6 @@ def mock_directions_car_with_rate_limiter(redis, mock_directions_car):
 
     # reset rate_limiter to default redis connection url
     rate_limiter.redis_pool = None
-
-
-@pytest.fixture
-def mock_directions_public_transport():
-    with override_settings({"COMBIGO_API_BASE_URL": "http://api.test"}):
-        fixture_path = os.path.join(
-            os.path.dirname(__file__),
-            "fixtures/directions",
-            "combigo_v1.1_publictransport.json",
-        )
-        with responses.RequestsMock() as rsps:
-            rsps.add(
-                "POST",
-                re.compile(r"^http://api.test/journey"),
-                status=200,
-                json=json.load(open(fixture_path)),
-            )
-            yield rsps
 
 
 @freeze_time("2018-06-14 8:30:00", tz_offset=0)
@@ -112,100 +93,15 @@ def test_direction_car_with_ids(mock_directions_car):
     assert "2.34023,48.89007;2.32658,48.85992" in mocked_request_url
 
 
-def test_direction_public_transport(mock_directions_public_transport):
-    client = TestClient(app)
-    response = client.get(
-        "http://localhost/v1/directions/2.3402355%2C48.8900732%3B2.3688579%2C48.8529869",
-        params={"language": "fr", "type": "publictransport"},
-    )
-
-    assert response.status_code == 200
-
-    response_data = response.json()
-    assert response_data["status"] == "success"
-    assert all(r["geometry"] for r in response_data["data"]["routes"])
-
-    route = response_data["data"]["routes"][0]
-    assert route["start_time"] == "2020-01-01T01:00:00+01:00"
-    assert route["end_time"] == "2020-01-01T01:30:00+01:00"
-
-    geometry = route["geometry"]
-    assert geometry["type"] == "FeatureCollection"
-    assert geometry["features"][1]["properties"] == {
-        "leg_index": 1,
-        "direction": "Mairie d'Issy (Issy-les-Moulineaux)",
-        "lineColor": "007852",
-        "mode": "SUBWAY",
-        "num": "12",
-        "network": "RATP",
-    }
-
-    summary = route["summary"]
-    assert list(map(lambda part: part["mode"], summary)) == [
-        "WALK",
-        "SUBWAY",
-        "WALK",
-        "SUBWAY",
-        "WALK",
-    ]
-
-    # Walk summary
-    assert summary[0]["info"] is None
-    assert summary[0]["distance"] > 0
-    assert summary[0]["duration"] > 0
-
-    # Subway summary
-    assert summary[1]["info"]["num"] == "12"
-    assert summary[1]["info"]["direction"] == "Mairie d'Issy (Issy-les-Moulineaux)"
-    assert summary[1]["info"]["lineColor"] == "007852"
-    assert summary[1]["info"]["network"] == "RATP"
-
-    # Subway leg
-    leg = route["legs"][1]
-    assert leg["from"] == {
-        "id": "1:4:43789",
-        "name": "Lamarck-Caulaincourt",
-        "location": [2.339149, 48.889738],
-    }
-    assert leg["to"] == {"id": "1:4:43790", "name": "Concorde", "location": [2.321412, 48.865489]}
-    assert len(leg["stops"]) == 7
-
-
-def test_directions_public_transport_restricted_areas():
-    client = TestClient(app)
-
-    # Paris - South Africa
-    response = client.get(
-        "http://localhost/v1/directions/2.3211757,48.8604893;22.1741215,-33.1565800",
-        params={"language": "fr", "type": "publictransport"},
-    )
-    assert response.status_code == 422
-
-    # Pekin
-    response = client.get(
-        "http://localhost/v1/directions/116.2945000,39.9148800;116.4998847,39.9091405",
-        params={"language": "fr", "type": "publictransport"},
-    )
-    assert response.status_code == 422
-
-    #  Washington - New York
-    response = client.get(
-        "http://localhost/v1/directions/-74.0308525,40.7697215;-77.0427329,38.8581794",
-        params={"language": "fr", "type": "publictransport"},
-    )
-    assert response.status_code == 422
-
-
 def test_directions_not_configured():
     with override_settings(
         {
-            "QWANT_DIRECTIONS_API_BASE_URL": None,
             "MAPBOX_DIRECTIONS_ACCESS_TOKEN": None,
         }
     ):
         client = TestClient(app)
         response = client.get(
-            "http://localhost/v1/directions/2.3402355%2C48.8900732%3B2.3688579%2C48.8529869",
+            "http://localhost/v1/directions/" "2.3402355%2C48.8900732%3B2.3688579%2C48.8529869",
             params={"language": "fr", "type": "driving"},
         )
         assert response.status_code == 501
@@ -216,7 +112,7 @@ def test_directions_rate_limiter(limiter_test_normal, mock_directions_car_with_r
     # rate limiter is triggered after 30 req/min by default
     for _ in range(40):
         response = client.get(
-            "http://localhost/v1/directions/2.3402355%2C48.8900732%3B2.3688579%2C48.8529869",
+            "http://localhost/v1/directions/" "2.3402355%2C48.8900732%3B2.3688579%2C48.8529869",
             params={"language": "fr", "type": "driving"},
             headers={"x-client-hash": "test-client-hash-value"},
         )
