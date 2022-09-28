a901e3c47878a8193321fcd68b4c130fb6e62027
sdirollo <sebastien.dirollo@gmail.com>
2022-09-28 14:23:37 +0200

    remove brand search by default for osm places bbox search

--- idunn/datasources/osm.py
+++ idunn/datasources/osm.py
@@ -52,22 +52,22 @@ class Osm(Datasource):
 
     async def get_places_bbox(self, params) -> list:
         """Get places within a given Bbox"""
-        if params.q:
-            # Default source (OSM) with query
+        if params.raw_filter or params.category:
+            if params.raw_filter:
+                filters = [MimirPoiFilter.from_url_raw_filter(f) for f in params.raw_filter]
+            else:
+                filters = [f for c in params.category for f in c.raw_filters()]
+            bbox_places = await run_in_threadpool(
+                fetch_es_pois,
+                "poi",
+                filters=filters,
+                bbox=params.bbox,
+                max_size=params.size,
+            )
+            return [OsmPOI(p["_source"]) for p in bbox_places]
+        else:
+            # if brand
             bragi_response = await bragi_client.pois_query_in_bbox(
                 query=params.q, bbox=params.bbox, lang=params.lang, limit=params.size
             )
             return [BragiPOI(PoiSource.OSM, f) for f in bragi_response.get("features", [])]
-
-        if params.raw_filter:
-            filters = [MimirPoiFilter.from_url_raw_filter(f) for f in params.raw_filter]
-        else:
-            filters = [f for c in params.category for f in c.raw_filters()]
-        bbox_places = await run_in_threadpool(
-            fetch_es_pois,
-            "poi",
-            filters=filters,
-            bbox=params.bbox,
-            max_size=params.size,
-        )
-        return [OsmPOI(p["_source"]) for p in bbox_places]
