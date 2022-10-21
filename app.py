f25dc6220aed8186623fa9a21ef8901d2e2ca93f
Pierre Jackman <p.jackman@qwant.com>
2022-10-21 14:28:52 +0200

    fixup

--- app.py
+++ app.py
@@ -10,15 +10,14 @@ from urllib.parse import urlparse
 
 # Setup docs settings
 base_url = settings["BASE_URL"]
-# path_prefix = settings["URL_PATH_PREFIX"]
-# root_path = urlparse(base_url).path.rstrip("/")
-root_path = "/maps/detail"
+path_prefix = settings["URL_PATH_PREFIX"]
+root_path = urlparse(base_url).path.rstrip("/")
 docs_settings = {}
 if not settings["DOCS_ENABLED"]:
     docs_settings.update({"openapi_url": None, "redoc_url": None, "docs_url": None})
 else:
     docs_settings.update(
-        {"openapi_url": "/openapi.json", "redoc_url": "/redoc", "docs_url": "/docs"}
+        {"openapi_url": "/openapi.json", "redoc_url": "/maps/detail/redoc", "docs_url": "/maps/detail/docs"}
     )
 
 # Setup FastAPI app
@@ -26,7 +25,7 @@ app = FastAPI(
     title="Idunn", version="0.2", debug=__name__ == "__main__", root_path=root_path, **docs_settings
 )
 v1_routes = get_api_urls(settings)
-app.include_router(APIRouter(routes=v1_routes))  # , prefix=path_prefix)
+app.include_router(APIRouter(routes=v1_routes), prefix=path_prefix)
 
 # Configure CORS
 app.add_middleware(
